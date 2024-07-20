be664dc6eaf3cd3cfb2ce5e900a60169edf8193d
haseryo0403 <ryo.hasegawa.work@gmail.com>
2024-07-20 16:20:10 +0900

    複数追加。説明省きます

--- TheShoppinngList/app/src/main/java/eu/tutorials/theshoppinnglist/MainActivity.kt
+++ TheShoppinngList/app/src/main/java/eu/tutorials/theshoppinnglist/MainActivity.kt
@@ -9,7 +9,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
 import eu.tutorials.theshoppinnglist.ui.theme.TheShoppinngListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //ShoppingListApp()
+                    Navigation()
                 }
             }
         }
@@ -30,17 +37,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Navigation(){
+    val navController = rememberNavController()
+    val viewModel: LocationViewModel = viewModel()
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheShoppinngListTheme {
-        Greeting("Android")
+    NavHost(navController, startDestination = "shoppinglistscreen"){
+        composable("shoppinglistscreen"){
+            ShoppingListApp(
+                locationUtils = locationUtils,
+                viewModel = viewModel,
+                navController = navController,
+                context = context,
+                address = viewModel.address.value.firstOrNull()?.formatted_address ?:"No Address"
+            )
+        }
+
+        dialog("locationscreen"){backstack->
+            viewModel.location.value?. let {
+                LocationSelectionScreen(location = it, onLocationSelected = {locationdata->
+                    viewModel.fetchAddress("${locationdata.latitude}, ${locationdata.longitude}")
+                    navController.popBackStack()
+                })
+            }
+        }
     }
 }
