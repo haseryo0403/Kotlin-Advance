cdd7b7a885fd29f38291e1497f6726eefde220c3
haseryo0403 <ryo.hasegawa.work@gmail.com>
2024-07-23 18:43:18 +0900

    複数ファイルを講座の進行に伴い追加。

--- TheShoppinngList/app/src/main/java/eu/tutorials/theshoppinnglist/ShoppingList.kt
+++ TheShoppinngList/app/src/main/java/eu/tutorials/theshoppinnglist/ShoppingList.kt
@@ -121,6 +121,7 @@ fun ShoppingListApp(
                             editedItem?.let {
                                 it.name = editedName
                                 it.quantity = editedQuantity
+                                it.address = address
                             }
                         }
                     )
@@ -194,7 +195,8 @@ fun ShoppingListApp(
                                 val newItem = ShoppingItem(
                                     id = sItems.size + 1,
                                     name = itemName,
-                                    quantity = itemQuantity.toInt()
+                                    quantity = itemQuantity.toInt(),
+                                    address = address
                                 )
                                 sItems = sItems + newItem
                                 showDialog = false
