594fcd7ff7df47725bafc71301a9eaf07914788a
haseryo0403 <ryo.hasegawa.work@gmail.com>
2024-07-30 21:34:19 +0900

    MusicAppUIを追加。

--- MusicAppUI/app/src/main/java/eu/tutorial/musicappui/ui/theme/MainView.kt
+++ MusicAppUI/app/src/main/java/eu/tutorial/musicappui/ui/theme/MainView.kt
@@ -1,6 +1,12 @@
 package eu.tutorial.musicappui.ui.theme
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -8,12 +14,30 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import eu.tutorial.musicappui.MainViewModel
+import eu.tutorial.musicappui.Screen
+import eu.tutorial.musicappui.screensInDrawer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -23,6 +47,21 @@ fun MainView(){
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+    val viewModel: MainViewModel = viewModel()
+
+    // Allows us to find out which "View" we currently are
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val currentScreen = remember{
+        viewModel.currentScreen.value
+    }
+
+    val title = remember{
+        // change that to currentScreen.title
+        mutableStateOf(currentScreen.title)
+    }
 
     Scaffold(
         topBar = {
@@ -37,16 +76,77 @@ fun MainView(){
                     Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
                 }}
                 )
+        },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)){
+                items(screensInDrawer){
+                    item->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if (item.dRoute == "add_account"){
+                            // open dialog
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
         }
     ) {
-        Text(text = "Text", modifier = Modifier.padding(it))
+        Navigation(navController = controller, viewModel = viewModel,pd = it)
     }
 }
 
 @Composable
 fun DrawerItem(
    selected: Boolean,
+   item: Screen.DrawerScreen,
    onDrawerItemClicked: () -> Unit
 ){
+    val background = if(selected) Color.DarkGray else Color.White
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .clickable {
+                onDrawerItemClicked()
+            }
+    ){
+        Icon(
+            painter = painterResource(id = item.icon), 
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
 
+@Composable
+fun Navigation(
+    navController: NavController,
+    viewModel: MainViewModel,
+    pd: PaddingValues
+){
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.DrawerScreen.AddAccount.route,
+        modifier = Modifier.padding(pd)
+        ){
+
+        composable(Screen.DrawerScreen.AddAccount.route){
+
+        }
+
+        composable(Screen.DrawerScreen.Subscription.route){
+            
+        }
+
+    }
 }
